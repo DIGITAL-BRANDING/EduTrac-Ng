f7bd6d9d5a65c69e7b3d78807df32ce4e0cb271c
YOUNG9774 <musasunusi701.com>
2026-08-07 01:00:34 +0100

    Update Base Url

--- client/js/config.js
+++ client/js/config.js
@@ -2,5 +2,5 @@
 window.__EDUTRAC_CONFIG__ = {
   SUPABASE_URL:      'https://nbgdjwuoiglkrmdeezsk.supabase.co',
   SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5iZ2Rqd3VvaWdsa3JtZGVlenNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQxODM5ODgsImV4cCI6MjA4OTc1OTk4OH0.x2pGldYnTMZSNMARQ2H4IBIWrUL2OtVQVOoWzkhU_Xg',
-  API_BASE_URL:      'https://edutrac-ng-production.up.railway.app'
+  API_BASE_URL:      'https://edutrac-ng-production-ceb1.up.railway.app'
 };
